bfa52873d2ad906260f69c179343e63b739cf4fb
mrmclaine <lypovskyi@gmail.com>
2025-04-09 00:29:00 +0200

    feat: add error handler

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './common/swagger/setup-swagger.config';
+import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.useGlobalFilters(new AllExceptionsFilter());
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     setupSwagger(app);
 
     await app.listen(process.env.PORT ?? 3000);
